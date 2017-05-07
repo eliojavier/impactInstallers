274a0c6d0c96a2e066549f35e832d4616d70a1b9
jess27p <jess27p@gmail.com>
2017-05-07 02:20:10 -0400

    login mejorado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AdminComponent} from './admin/admin.component';
-import { LoginComponent} from './login/login.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   imports: [
