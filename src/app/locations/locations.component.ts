e356b3bd5a7d2834052f8fb43e09643fef58cad9
jess27p <jess27p@gmail.com>
2017-05-09 23:43:23 -0400

    datatables locations

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationsComponent implements OnInit {
 
+  selected: any[] = [];
+  rows = [
+    { id: 1, name: 'Doral', state: 'Florida', city: 'Miami', postalCode: '333232', coordinates: '-121.23, 23.32'},
+    { id: 2, name: 'Brickell', state: 'Florida', city: 'Miami', postalCode: '333132', coordinates: '-111.23, 25.32'},
+    { id: 3, name: 'Miami Beach', state: 'Florida', city: 'Miami', postalCode: '333245', coordinates: '-191.29, 23.32'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(event) {
+    console.log('Event: select', event, this.selected);
+  }
+
+  delete() {
+    console.log('delete');
+  }
 }
