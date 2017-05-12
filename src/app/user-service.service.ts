3839db6945f7659ee72964283b873c8f39f4d617
jess27p <jess27p@gmail.com>
2017-05-12 00:55:45 -0400

    get users

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, URLSearchParams, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class UserServiceService {
+public baseURI = 'http://localhost:8000/'
+  constructor(public http: Http) {
 
-  constructor() { }
+  }
+
+  getUsers() {
+    return this.http.get(this.baseURI + 'api/users')
+      .map((response: Response) => response.json())
+      .catch(this.errorHandler);
+  }
+
+  errorHandler(error: Response) {
+    console.log(error);
+    return Observable.throw(error || 'server error');
+  }
 
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
-
+import { UserServiceService } from '../user-service.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
+  providers: [UserServiceService]
 })
 export class UsersComponent implements OnInit {
 
+  public errorMsg: string;
   selected: any[] = [];
-  rows = [
-    { id: 1, name: 'Elio', lastName: 'Acosta', documentId: '12221', email: 'eliojavier@gmail.com', address: 'San Martin', phone: '2782922' },
-    { id: 2, name: 'Jessica', lastName: 'Perez', documentId: '12223', email: 'jess@gmail.com', address: 'San Antonio', phone: '111111' }
-  ];
+  rows = [];
+    // { id: 1, name: 'Elio', lastName: 'Acosta', documentId: '12221', email: 'eliojavier@gmail.com', address: 'San Martin', phone: '2782922' },
+    // { id: 2, name: 'Jessica', lastName: 'Perez', documentId: '12223', email: 'jess@gmail.com', address: 'San Antonio', phone: '111111' }
+  // ];
 
   public registerForm = this.formBuilder.group({
     first_name: ['', Validators.required],
@@ -24,11 +26,20 @@ export class UsersComponent implements OnInit {
     address: ['', [Validators.required]],
   });
 
-  constructor(public formBuilder: FormBuilder) {
-
+  constructor(public formBuilder: FormBuilder, private userService: UserServiceService) {
   }
 
   ngOnInit() {
+    this.userService.getUsers()
+      .subscribe(
+        response => {
+          if (response) {
+            console.log(response);
+            this.rows = response.users;
+          }
+        },
+        error => this.errorMsg = error,
+      );
   }
 
   submitForm() {
